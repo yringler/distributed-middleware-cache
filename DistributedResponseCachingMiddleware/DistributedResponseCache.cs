135f0256717ffc870425ded1bce008b8cf074e74
Yehuda Ringler <yehuda.ringler@chabad.org>
2020-05-05 17:11:42 -0400

    Tests pass: read and write

---
+++ DistributedResponseCachingMiddleware/DistributedResponseCache.cs
@@ -0,0 +1,105 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.ResponseCaching.Internal;
+using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.Extensions.Primitives;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace DistributedResponseCachingMiddleware.DistrubtedReponseCache
+{
+	public class DistributedResponseCache : IResponseCache
+	{
+		private readonly IDistributedCache cache;
+
+		public DistributedResponseCache(IDistributedCache cache)
+		{
+			this.cache = cache;
+		}
+
+		public IResponseCacheEntry Get(string key)
+		{
+			return GetAsync(key).Result;
+		}
+
+		public async Task<IResponseCacheEntry> GetAsync(string key)
+		{
+			var bytes = await cache.GetAsync(key);
+			using var stream = new MemoryStream(bytes);
+			var data = await JsonSerializer.DeserializeAsync<SerializableCachedResponse>(stream);
+
+			return data.ToCachedResponse();
+		}
+
+		public void Set(string key, IResponseCacheEntry entry, TimeSpan validFor)
+		{
+			SetAsync(key, entry, validFor).Wait();
+		}
+
+		public async Task SetAsync(string key, IResponseCacheEntry entry, TimeSpan validFor)
+		{
+			var cachedResponse = entry as CachedResponse;
+
+			if (entry == null)
+			{
+				throw new Exception("Oh goodness, this isn't working out. Sorry about that.");
+			}
+
+			using var stream = new MemoryStream();
+			await JsonSerializer.SerializeAsync(stream, SerializableCachedResponse.From(cachedResponse));
+
+
+			using var reader = new StreamReader(stream);
+
+			stream.Position = 0;
+			Console.WriteLine(reader.ReadToEnd());
+
+			await cache.SetAsync(key, stream.ToArray(), new DistributedCacheEntryOptions
+			{
+				AbsoluteExpirationRelativeToNow = validFor
+			});
+		}
+	}
+
+	internal class SerializableCachedResponse
+	{
+		public DateTimeOffset Created { get; set; }
+
+		public int StatusCode { get; set; }
+
+		public List<KeyValuePair<string, List<string>>> Headers { get; set; }
+
+		public byte[] Body { get; set; }
+
+		public static SerializableCachedResponse From(CachedResponse cachedResponse)
+		{
+			using var stream = new MemoryStream();
+			cachedResponse.Body.CopyTo(stream);
+
+			return new SerializableCachedResponse
+			{
+				Created = cachedResponse.Created,
+				StatusCode = cachedResponse.StatusCode,
+				Headers = cachedResponse.Headers
+					.Select(header => new KeyValuePair<string, List<string>>(header.Key, header.Value.ToList()))
+					.ToList(),
+				Body = stream.ToArray()
+			};
+		}
+
+		public CachedResponse ToCachedResponse()
+		{
+			return new CachedResponse
+			{
+				Created = Created,
+				Body = new MemoryStream(Body),
+				Headers = new HeaderDictionary(Headers.ToDictionary(
+					header => header.Key,
+					header => header.Value.Any() ? new StringValues(header.Value.ToArray()) : StringValues.Empty))
+			};
+		}
+	}
+}
---
+++ DistributedResponseCachingMiddlewareTests/Tests.cs
@@ -0,0 +1,69 @@
+using NUnit.Framework;
+using Moq;
+using Microsoft.Extensions.Caching.Distributed;
+using DistributedResponseCachingMiddleware.DistrubtedReponseCache;
+using Microsoft.AspNetCore.ResponseCaching.Internal;
+using System;
+using System.IO;
+using Microsoft.AspNetCore.Http;
+using System.Collections.Generic;
+using Microsoft.Extensions.Primitives;
+using System.Threading;
+
+namespace DistributedResponseCachingMiddlewareTests
+{
+	[TestFixture]
+	public class Tests
+	{
+		[Test]
+		public void CanWriteCache()
+		{
+			var mockDistributedCache = new Mock<IDistributedCache>();
+			var responseCache = new DistributedResponseCache(mockDistributedCache.Object);
+
+			using var stream = new MemoryStream();
+			using (var writer = new StreamWriter(stream, leaveOpen: true))
+			{
+				writer.Write("data");
+			}
+			stream.Position = 0;
+
+			Assert.That(() => responseCache.Set("test", new CachedResponse
+			{
+				Body = stream,
+				Headers = new HeaderDictionary(new Dictionary<string, StringValues>
+				{
+					["x-test-header"] = "testing...testing"
+				})
+			}, TimeSpan.FromMinutes(1)), Throws.Nothing);
+		}
+
+		[Test]
+		public void CanReadCache()
+		{
+			string json = "{\"Created\":\"0001-01-01T00:00:00+00:00\",\"StatusCode\":0,\"Headers\":[{\"Key\":\"x-test-header\",\"Value\":[\"testing...testing\"]}],\"Body\":\"ZGF0YQ==\"}";
+			using var stream = new MemoryStream();
+			using var writer = new StreamWriter(stream, leaveOpen: true);
+			writer.Write(json);
+			writer.Flush();
+
+			stream.Position = 0;
+
+			var mockDistributedCache = new Mock<IDistributedCache>();
+			mockDistributedCache.Setup(cache => cache.GetAsync(It.IsAny<string>(), It.IsAny<CancellationToken>()))
+				.ReturnsAsync(stream.ToArray());
+
+			var responseCache = new DistributedResponseCache(mockDistributedCache.Object);
+
+			var savedValue = responseCache.Get("somekey") as CachedResponse;
+
+			Assert.That(savedValue, Is.Not.Null);
+			Assert.That(savedValue.Headers["x-test-header"], Is.EqualTo("testing...testing"));
+
+			using var reader = new StreamReader(savedValue.Body);
+			string body = reader.ReadToEnd();
+
+			Assert.That(body, Is.EqualTo("data"));
+		}
+	}
+}
