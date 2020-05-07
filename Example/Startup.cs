8a9979e0c8da2f3f269bd42e5eff9374a48736c6
Yehuda Ringler <yehuda.ringler@chabad.org>
2020-05-07 11:04:07 -0400

    start adding example

---
+++ Example/Startup.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace Example
+{
+	public class Startup
+	{
+		public Startup(IConfiguration configuration)
+		{
+			Configuration = configuration;
+		}
+
+		public IConfiguration Configuration { get; }
+
+		// This method gets called by the runtime. Use this method to add services to the container.
+		public void ConfigureServices(IServiceCollection services)
+		{
+			services.AddMvc()
+				.AddXmlSerializerFormatters();
+			services.AddControllers();
+			services.AddResponseCaching();
+		}
+
+		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+		{
+			if (env.IsDevelopment())
+			{
+				app.UseDeveloperExceptionPage();
+			}
+
+			app.UseRouting();
+
+			app.Use(async (context, next) =>
+			{
+				context.Response.Headers[Microsoft.Net.Http.Headers.HeaderNames.Vary] =
+	new string[] { "Accept-Encoding", "Accept" };
+
+				await next();
+			});
+
+			app.UseResponseCaching();
+
+			app.UseEndpoints(endpoints =>
+			{
+				endpoints.MapControllers();
+			});
+		}
+	}
+}
